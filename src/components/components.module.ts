2c972af17864280d9ee45cc909beb8099a8a2529
OthmanAyman <othman.ayman47@gmail.com>
2018-11-29 11:07:06 +0200

    add src folder to new branch

---
+++ src/components/components.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { IonicModule } from 'ionic-angular';
+import { VideoComponent } from './video/video';
+
+@NgModule({
+	declarations: [VideoComponent],
+	imports: [IonicModule],
+	exports: [VideoComponent]
+})
+export class ComponentsModule {}
---
+++ src/pages/fifth-year/fifth-year.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import {FifthYearPage} from "./fifth-year";
+@NgModule({
+  declarations: [
+    FifthYearPage
+  ],
+  imports: [
+    IonicPageModule.forChild(FifthYearPage),
+  ],
+})
+export class FifthYearPageModule {}
